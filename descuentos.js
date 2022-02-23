76880da314d8373216e3586bd150c3be1bd75aed
SpectrumGitHub2 <aaru090903@gmail.com>
2022-02-22 22:52:30 -0500

    Avance 2 descuentos

--- descuentos.js
+++ descuentos.js
@@ -1,12 +1,29 @@
-const precio = 115;
-const descuento = 15;
+// const precio = 115;
+// const descuento = 15;
 
-const porcentajeDescuento = 100 - descuento;
-const precioFinal = (precio * porcentajeDescuento)/100;
+function calcularDescuento(precio, descuento){
+    const porcentajeDescuento = 100 - descuento;
+    const precioFinal = (precio * porcentajeDescuento)/100;
 
-console.log({
-    precio,
-    descuento,
-    porcentajeDescuento,
-    precioFinal,
-});
+    return precioFinal;
+}
+
+// console.log({
+//     precio,
+//     descuento,
+//     porcentajeDescuento,
+//     precioFinal,
+// });
+
+function buttonPriceDiscount(){
+    let price = document.getElementById('InputPrice');
+    const priceValue = price.value;
+
+    let discount = document.getElementById('InputDiscount');
+    const discountValue = discount.value;
+
+    const finalPrice = calcularDescuento(priceValue, discountValue);
+    const resultP = document.getElementById('finalPrice');
+
+    resultP.innerText = "El precio final es: S/ " + finalPrice;
+}
